91976c74606680c7326cf68e5a4a7cdea10ccb0a
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-02 16:44:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  var currentLevel=document.getElementsByTagName('div');
+
+  for (var i = 0; i<currentLevel.length;i++){
+    if (currentLevel[i].className==='target'){
+      return currentLevel[i];
+    }
+  }
+}
+
+function deepestChild(){
+  var currentLevel=document.getElementById('grand-node').querySelectorAll('div');
+  var answerLength=currentLevel.length-1;
+  return currentLevel[answerLength];
+}
+
+function increaseRankBy(num){
+  var rankedListsA= document.querySelectorAll('ul.ranked-list li');
+
+  for (var i =0;i<rankedListsA.length;i++){
+    rankedListsA[i].innerHTML= parseInt(rankedListsA[i].innerHTML)+num;
+    console.log(rankedListsA[i].innerHTML);
+  }
+}

4106e2a7b8ff788059c91e3b1b527dc393f4e492
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-02 16:28:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function increaseRankBy(num){
       current += num;
       console.log(parseInt(rankedListsA[i].children[j].innerHTML))
       console.log(rankedListsA[0])
-      // console.log(current);
+      console.log(current);
     }
   }
 }

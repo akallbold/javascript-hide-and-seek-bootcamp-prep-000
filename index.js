782f888f4b7277484c37e8e173304c4cf115c4e6
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-02 15:27:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  var currentLevel=document.getElementsByTagName('div');
+
+  for (var i = 0; i<currentLevel.length;i++){
+    if (currentLevel[i].className==='target'){
+      return currentLevel[i];
+    }
+  }
+}
+
+function deepestChild(){
+  var currentLevel=document.getElementById('grand-node');
+
+  while (currentLevel.children){
+    currentLevel+=currentLevel.children[0];
+  } return currentLevel.innerHTML;
+}
+
+function increaseRankBy(num){
+  var rankedListsA= document.getElementsByClassName('ranked-list')
+
+  for (var i =0;i<rankedListsA.length;i++){
+    for(var j =0;j<rankedListsA[i].children.length;j++){
+      var current= parseInt(rankedListsA[i].children[j].innerHTML);
+      // console.log(current);
+      current += num;
+      // console.log(current);
+    }
+  }
+}
